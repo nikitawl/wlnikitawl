39593ab1e0c01ec9030b2c1fe21f7de2f8727547
nikitawl <48116145+nikitawl@users.noreply.github.com>
2020-03-29 20:20:22 +0500

    Update and rename README.md to Positive.py

---
+++ Positive.py
@@ -0,0 +1,7 @@
+a = 0
+number = int(input("Write a positive number: "))
+while number <= a:
+    print('Sorry, but that is not a positive number:')
+    number = int(input('Write a positive number:'))
+if number > a :
+    print("Thank you, for your support, you have just picked the  number of ",number)
